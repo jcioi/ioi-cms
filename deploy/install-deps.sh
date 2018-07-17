b4a13f6f3863415b4e0b8dccdcefaa492171b9b2
Sorah Fukumori <sorah@cookpad.com>
2018-07-18 08:05:17 +0900

    force python3

--- deploy/install-deps.sh
+++ deploy/install-deps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -xe
 cd /opt/cms
-python -m venv /opt/cms_venv
+python3 -m venv /opt/cms_venv
 source /opt/cms_venv/bin/activate
 pip install --no-cache-dir -r requirements.txt
 ./setup.py install
