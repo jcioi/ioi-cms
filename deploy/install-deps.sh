26cf2de11770c42b9996f9f9f8f1efc8e24d4757
Sorah Fukumori <sorah@cookpad.com>
2018-07-18 09:25:23 +0900

    compile_catalog

--- deploy/install-deps.sh
+++ deploy/install-deps.sh
@@ -3,5 +3,5 @@ set -xe
 python3 -m venv /opt/cms_venv
 source /opt/cms_venv/bin/activate
 pip install --no-cache-dir -r requirements.txt
+./setup.py compile_catalog
 ./setup.py install
-./prerequisites.py build_l10n
